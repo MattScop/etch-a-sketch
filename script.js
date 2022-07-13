867c176bc79284ed0b95b5bfd02aad60497177f6
MattScop <matteo.scopece@outlook.com>
2022-07-13 11:06:12 +0200

    Project completed
    I went a bit further and changed the functionality of the mouse so that
    it only "draws" when hovering + mouse left click hold. I find it to be
    a bit more comfortable to play with.

--- script.js
+++ script.js
@@ -63,20 +63,102 @@ Don’t try making them by hand with copy and pasting in your HTML file!
 
         // ---------METHOD D: GRID---------
         const container = document.querySelector('.container');
-        container.style.cssText = 'width: 100vw; height: 100vh; display: grid; grid-template-columns: repeat(16, 1fr);';
+
+        // Set Initial Values for the Grid on page load
+        function initValue() {
+        const gridSideValue = 16; // number of squares on one side of the grid
+        const grid = gridSideValue * gridSideValue;
+
+        container.style.setProperty('--gridSideValue', gridSideValue); // set custom property to be able to modify the grid later
         
+        // Insert the Grid in the Container
         let i = 0;
-        while (i < 256) {
-            const gridPiece = document.createElement('div');
-            gridPiece.className = 'grid-piece';
-            container.appendChild(gridPiece);
+        while (i < grid) {
+            const gridElement = document.createElement('div');
+            gridElement.className = 'grid-element';
+            gridElement.style.outline = '1px solid rgb(0 0 0 / 5%)'
+            container.appendChild(gridElement);
             i++
         }
 
-// Add Mouse Hover Event
-const divGrid = document.getElementsByClassName('grid-piece');
-Array.from(divGrid).forEach(gridDiv => {
-    gridDiv.addEventListener('mouseenter', (e) => {
-        e.target.style.backgroundColor = 'black';
-    })
-})
+        // Add Mouse Event
+        const gridElements = document.querySelectorAll('.grid-element');
+        const arrGrid = Array.from(gridElements);
+
+        for (let j = 0; j < arrGrid.length; j++) {
+            arrGrid[j].addEventListener('mousedown', (e) => {
+                e.preventDefault()
+                e.target.style.backgroundColor = 'black';
+            }) // Color first cell on mouse down
+
+            arrGrid[j].addEventListener('mouseenter', (e) => {
+                if (e.which === 1) {
+                e.target.style.backgroundColor = 'black';
+                } // Change BG only if mouse is down 
+            })
+        }
+
+    }
+
+    initValue()
+
+
+
+/* 
+STEP 4: Add a button to the top of the screen that will send the user a popup asking for the number of squares per side for the new grid. Once entered, the existing grid should be removed and a new grid should be generated in the same total space as before (e.g. 960px wide) so that you’ve got a new sketch pad. Tip: Set the limit for the user input to a maximum of 100. A larger number of squares results in more computer resources being used, potentially causing delays, freezing, or crashing that we want to prevent.
+ */
+
+// Add & Style Change Grid Button
+const btn = document.createElement('button');
+btn.setAttribute('style', 'padding: 10px 25px; font-size: 1.1rem;');
+const btnText = document.createTextNode('Change Grid Style');
+btn.appendChild(btnText);
+document.body.insertBefore(btn, container);
+
+    // Add Button Functionality
+    btn.addEventListener('click', askGridStyle);
+
+function askGridStyle() {
+    // Prompt
+    const gridSideValue = Number(prompt('Insert a new value for the grid (max: 100)', 16));
+    if (gridSideValue > 100) {
+        alert('Maximum input is 100');
+        return
+    }
+    
+    // Clear Old Grid Before Inserting New One
+    const previousGridLength = container.childElementCount;
+    for (let i = 0; i < previousGridLength; i++) {
+        container.removeChild(container.lastChild);
+    }
+
+    // Add New Grid
+    const grid = gridSideValue * gridSideValue;
+    container.style.setProperty('--gridSideValue', gridSideValue);
+    let i = 0;
+        while (i < grid) {
+            const gridElement = document.createElement('div');
+            gridElement.className = 'grid-element';
+            gridElement.style.outline = '0.5px solid rgb(0 0 0 / 5%)'
+            container.appendChild(gridElement);
+            i++
+        }
+
+    // Add Mouse Event
+    const gridElements = document.querySelectorAll('.grid-element');
+    const arrGrid = Array.from(gridElements);
+
+    for (let j = 0; j < arrGrid.length; j++) {
+        arrGrid[j].addEventListener('mousedown', (e) => {
+            e.preventDefault()
+            e.target.style.backgroundColor = 'black';
+        }) // Color first cell on mouse click
+
+        arrGrid[j].addEventListener('mouseenter', (e) => {
+            if (e.which === 1) {
+            e.target.style.backgroundColor = 'black';
+            } // Change BG only if mouse is down 
+        })
+    }
+
+}
